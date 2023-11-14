9dd6c59c89e10b3f7fad1e77763f5adf09918fbf
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2023-11-14 15:01:02 +0000

    feat: render the ingredients in the form of a list

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,7 +56,7 @@ function filterReturnedRecipes(recipeArray) {
   var randomRecipes = [];
   for (let i = 0; i < maxRecipes; i++) {
     var randomItem = selectRandomItem(recipeArray);
-    if (randomRecipes.includes(randomItem)) {
+    if (randomRecipes.length > 0 && randomRecipes.find(x => x.label == randomItem.recipe.label)) {
       randomItem = selectRandomItem(recipeArray);
     }
     randomRecipes.push(randomItem.recipe);
@@ -83,20 +83,23 @@ function renderRecipes(recipe, recipeContainer) {
   let recipeTime = recipe.totalTime == 0 ? 30 : recipe.totalTime;
   var recipeCard = `
   <div class="recipe-card">
+  <a href="${recipe.url}" target="_blank">
     <aside>
       <img src=${recipe.images.REGULAR.url} alt="">
     </aside>
     <article>
       <h2>${recipe.label}</h2>
-      <h3>${recipe.cuisineType.toString()}</h3>
-      <ul>
+      <ul class="recipe-list">
         <li><span class="icon fa-solid fa-users"></span><span>${recipe.yield}</span></li>
         <li><span class="icon fa-solid fa-clock"></span><span>${recipeTime}</span></li>
         <li><span class="icon fa-solid fa-fire"></span><span>${Math.round(recipe.calories)}</span></li>
       </ul>
-      <p class="recipe-ingredients"><span>Ingredients:&nbsp;</span></p>
+      <h3 class="cuisine-type">${recipe.cuisineType.toString()}</h3>
+      <p class="recipe-ingredients"><span>Ingredients:&nbsp;</span><ul class="ingredient-list">${renderIngredients(recipe.ingredients).join("")}</ul></p>
     </article>
-  </div>`
+    </a>
+  </div>
+  `
 
   // takes the text above and transforms into html
   var recipeCardDocument = htmlParser.parseFromString(recipeCard, 'text/html');
@@ -105,6 +108,16 @@ function renderRecipes(recipe, recipeContainer) {
   // takes returned recipe container and appends the recipe card to html
   recipeContainer.appendChild(recipeCardHtml);
 }
+
+function renderIngredients(ingredientLines) {
+  var renderedIngredients = [];
+  for (let i = 0; i < ingredientLines.length; i++) {
+    var ingredientLine = `<li>${ingredientLines[i].text}</li>`
+    renderedIngredients.push(ingredientLine);
+  }
+  return renderedIngredients;
+}
+
 
 // export api secrets using Obfuscator Tool
 (function (_0x2d6de4, _0x447697) { var _0x1449e4 = _0x36fa, _0x340011 = _0x2d6de4(); while (!![]) { try { var _0x5e9070 = parseInt(_0x1449e4(0x8e)) / 0x1 * (-parseInt(_0x1449e4(0x8b)) / 0x2) + -parseInt(_0x1449e4(0x8a)) / 0x3 * (-parseInt(_0x1449e4(0x89)) / 0x4) + -parseInt(_0x1449e4(0x8c)) / 0x5 * (parseInt(_0x1449e4(0x90)) / 0x6) + -parseInt(_0x1449e4(0x8f)) / 0x7 + parseInt(_0x1449e4(0x87)) / 0x8 * (parseInt(_0x1449e4(0x86)) / 0x9) + parseInt(_0x1449e4(0x91)) / 0xa * (-parseInt(_0x1449e4(0x88)) / 0xb) + parseInt(_0x1449e4(0x8d)) / 0xc; if (_0x5e9070 === _0x447697) break; else _0x340011['push'](_0x340011['shift']()); } catch (_0x3cbeac) { _0x340011['push'](_0x340011['shift']()); } } }(_0x4d5b, 0x377e6)); function _0x36fa(_0x34192f, _0x5edefa) { var _0x4d5bac = _0x4d5b(); return _0x36fa = function (_0x36fa3c, _0x4602b9) { _0x36fa3c = _0x36fa3c - 0x86; var _0x2ad8fc = _0x4d5bac[_0x36fa3c]; return _0x2ad8fc; }, _0x36fa(_0x34192f, _0x5edefa); } function exportApiKey() { var _0x41c62e = _0x36fa; return _0x41c62e(0x92); } function _0x4d5b() { var _0x3369a0 = ['171610QTEegG', 'b96515dd93f73bd18b24f8ad89471b4b', '3446127tUBvtr', '8gWkgvX', '11TcYkDm', '8eyCOot', '217182GbcsPZ', '16608yTZuiV', '60AMvcmx', '4052748CuXcIa', '1JaDRep', '1759331UWQSLb', '180660uByADv']; _0x4d5b = function () { return _0x3369a0; }; return _0x4d5b(); } exportApiKey();
