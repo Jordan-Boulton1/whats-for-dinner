f40d91c0c30a4265bb4916b599a25f38ee99a5d9
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2023-11-21 15:34:50 +0000

    feat: fetch recipe id from api and check for duplicates

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ function callApi(ingredientValue) {
       let recipeContainer = document.getElementById('returned-recipe-container');
       console.log(returnedRecipes)
       recipeContainer.innerHTML = "";
-      // loops through the 4 random recipes returned
+      // loops through the 8 random recipes returned
       for (let i = 0; i < returnedRecipes.length; i++) {
         renderRecipes(returnedRecipes[i], recipeContainer);
       }
@@ -57,9 +57,9 @@ function filterReturnedRecipes(recipeArray) {
   var maxRecipes = 8;
   var randomRecipes = [];
   for (let i = 0; i < maxRecipes; i++) {
-    var randomItem = selectRandomItem(recipeArray);
-    if (randomRecipes.length > 0 && randomRecipes.find(x => x.label == randomItem.recipe.label)) {
-      randomItem = selectRandomItem(recipeArray);
+    let randomItem = selectRandomItem(recipeArray);
+    if (randomRecipes.filter(e => e.Id === randomItem.recipe.Id).length > 0) {
+      var randomItem = selectRandomItem(recipeArray);
     }
     randomRecipes.push(randomItem.recipe);
   }
@@ -73,7 +73,11 @@ function filterReturnedRecipes(recipeArray) {
  */
 function selectRandomItem(recipeArray) {
   var randomRecipeIndex = Math.floor(Math.random() * recipeArray.length);
-  return recipeArray[randomRecipeIndex];
+  // get the recipe id from the api
+  let randomItem = recipeArray[randomRecipeIndex];
+  let randomItemId = randomItem.recipe.uri.substring(randomItem.recipe.uri.indexOf("_") + 1);
+  randomItem.recipe.Id = randomItemId;
+  return randomItem;
 }
 
 /**
@@ -84,8 +88,7 @@ function selectRandomItem(recipeArray) {
 function renderRecipes(recipe, recipeContainer) {
   let recipeTime = recipe.totalTime == 0 ? 30 : recipe.totalTime;
   var recipeCard = `
-  <div class="recipe-card">
-  <a href="${recipe.url}" target="_blank">
+  <div class="recipe-card" id="${recipe.Id}">
     <aside>
       <img src=${recipe.images.REGULAR.url} alt="">
     </aside>
@@ -97,11 +100,15 @@ function renderRecipes(recipe, recipeContainer) {
         <li><span class="icon fa-solid fa-fire"></span><span>${Math.round(recipe.calories)}</span></li>
       </ul>
       <h3 class="cuisine-type">${recipe.cuisineType.toString()}</h3>
-      <p class="recipe-ingredients"><ul class="ingredient-list">${renderIngredients(recipe.ingredients).join("")}</ul></p>
+      <button type="button" class="collapsible" onclick="expandIngredients(\``+ recipe.Id + `\`)">∨</button>
+      <div class="content" id="content-${recipe.Id}">
+        <p class="recipe-ingredients"><ul class="ingredient-list">${renderIngredients(recipe.ingredients).join("")}</ul></p>
+      </div>
     </article>
-    </a>
   </div>
   `
+
+
 
   // takes the text above and transforms into html
   var recipeCardDocument = htmlParser.parseFromString(recipeCard, 'text/html');
@@ -126,8 +133,6 @@ function renderIngredients(ingredientLines) {
 }
 
 // export api secrets using Obfuscator Tool
-(function (_0x2d6de4, _0x447697) { var _0x1449e4 = _0x36fa, _0x340011 = _0x2d6de4(); while (!![]) { try { var _0x5e9070 = parseInt(_0x1449e4(0x8e)) / 0x1 * (-parseInt(_0x1449e4(0x8b)) / 0x2) + -parseInt(_0x1449e4(0x8a)) / 0x3 * (-parseInt(_0x1449e4(0x89)) / 0x4) + -parseInt(_0x1449e4(0x8c)) / 0x5 * (parseInt(_0x1449e4(0x90)) / 0x6) + -parseInt(_0x1449e4(0x8f)) / 0x7 + parseInt(_0x1449e4(0x87)) / 0x8 * (parseInt(_0x1449e4(0x86)) / 0x9) + parseInt(_0x1449e4(0x91)) / 0xa * (-parseInt(_0x1449e4(0x88)) / 0xb) + parseInt(_0x1449e4(0x8d)) / 0xc; if (_0x5e9070 === _0x447697) break; else _0x340011['push'](_0x340011['shift']()); } catch (_0x3cbeac) { _0x340011['push'](_0x340011['shift']()); } } }(_0x4d5b, 0x377e6)); function _0x36fa(_0x34192f, _0x5edefa) { var _0x4d5bac = _0x4d5b(); return _0x36fa = function (_0x36fa3c, _0x4602b9) { _0x36fa3c = _0x36fa3c - 0x86; var _0x2ad8fc = _0x4d5bac[_0x36fa3c]; return _0x2ad8fc; }, _0x36fa(_0x34192f, _0x5edefa); } function exportApiKey() { var _0x41c62e = _0x36fa; return _0x41c62e(0x92); } function _0x4d5b() { var _0x3369a0 = ['171610QTEegG', 'b96515dd93f73bd18b24f8ad89471b4b', '3446127tUBvtr', '8gWkgvX', '11TcYkDm', '8eyCOot', '217182GbcsPZ', '16608yTZuiV', '60AMvcmx', '4052748CuXcIa', '1JaDRep', '1759331UWQSLb', '180660uByADv']; _0x4d5b = function () { return _0x3369a0; }; return _0x4d5b(); } exportApiKey();
-(function (_0x36f542, _0x2fc93c) { var _0x3aad74 = _0x12c6, _0x550034 = _0x36f542(); while (!![]) { try { var _0x5f07b3 = parseInt(_0x3aad74(0x1be)) / 0x1 + parseInt(_0x3aad74(0x1b8)) / 0x2 * (-parseInt(_0x3aad74(0x1c2)) / 0x3) + -parseInt(_0x3aad74(0x1bc)) / 0x4 * (-parseInt(_0x3aad74(0x1b6)) / 0x5) + parseInt(_0x3aad74(0x1bd)) / 0x6 * (parseInt(_0x3aad74(0x1c0)) / 0x7) + parseInt(_0x3aad74(0x1c3)) / 0x8 * (-parseInt(_0x3aad74(0x1bf)) / 0x9) + parseInt(_0x3aad74(0x1bb)) / 0xa * (parseInt(_0x3aad74(0x1ba)) / 0xb) + parseInt(_0x3aad74(0x1c1)) / 0xc * (-parseInt(_0x3aad74(0x1b9)) / 0xd); if (_0x5f07b3 === _0x2fc93c) break; else _0x550034['push'](_0x550034['shift']()); } catch (_0x2061c0) { _0x550034['push'](_0x550034['shift']()); } } }(_0x592a, 0x37228)); function exportApiId() { var _0x109e81 = _0x12c6; return _0x109e81(0x1b7); } function _0x12c6(_0x147adc, _0x9f25f7) { var _0x592abd = _0x592a(); return _0x12c6 = function (_0x12c6e3, _0x195f1f) { _0x12c6e3 = _0x12c6e3 - 0x1b6; var _0x27e2b4 = _0x592abd[_0x12c6e3]; return _0x27e2b4; }, _0x12c6(_0x147adc, _0x9f25f7); } exportApiId(); function _0x592a() { var _0x1133a3 = ['3203656QOOEiY', '6325CtkMQy', 'e0a6e02b', '2BtRqJz', '11986oBJsns', '11sgHKqK', '2922910qDhPSQ', '1028JMELSf', '42gJVTuc', '159491fTcSjd', '9ZueAIQ', '259112yjjrgp', '5088CNKXQj', '56346QEhlWq']; _0x592a = function () { return _0x1133a3; }; return _0x592a(); }
 
 function randomButton() {
   var randomItemsArray = new Array("chicken", "beef", "breadcrumbs", "tomato paste", "eggs", "sushi", "prawns", "wraps", "pasta", "rice", "fish", "peppers", "cheese", "curry");
