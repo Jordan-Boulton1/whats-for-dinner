73b1daeab1ea16fdae88f2c91c80e56cbcde4953
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2023-11-28 23:06:06 +0000

    fix: improve code readability and structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,34 @@
 const htmlParser = new DOMParser();
 loadCache();
 
-document.getElementById('search-btn').addEventListener("click", function (event) {
-  event.preventDefault();
-  let ingredientValue = document.getElementById("search-term").value;
-  console.log(ingredientValue);
-  callApi(ingredientValue);
-});
+function loadCache() {
+  let getRecipesFromCache = localStorage.getItem("cached-recipes");
+  if (getRecipesFromCache == null) {
+    return;
+  }
+  getRecipesFromCache = JSON.parse(getRecipesFromCache);
+  let recipeContainer = document.getElementById('returned-recipe-container');
+  recipeContainer.innerHTML = "";
+  renderRecipes(getRecipesFromCache, recipeContainer);
+}
 
 document.getElementById('random-btn').addEventListener("click", function (event) {
   event.preventDefault();
   randomButton();
+});
+
+function randomButton() {
+  document.getElementById("search-term").value = "";
+  let randomItemsArray = new Array("chicken", "beef", "breadcrumbs", "tomato paste", "eggs", "sushi", "prawns", "wraps", "pasta", "rice", "fish", "peppers", "cheese", "curry");
+  let getRandomItem = randomItemsArray[Math.floor(Math.random() * randomItemsArray.length)];
+
+  callApi(getRandomItem);
+}
+
+document.getElementById('search-btn').addEventListener("click", function (event) {
+  event.preventDefault();
+  let ingredientValue = document.getElementById("search-term").value;
+  callApi(ingredientValue);
 });
 
 function callApi(ingredientValue) {
@@ -24,44 +42,38 @@ function callApi(ingredientValue) {
   xhttp.open("GET", recipeUrl);
   xhttp.send();
   xhttp.responseType = "json";
+
   // onload processes the request, then when the request is processed we evaluate
   // the response by checking if the ready state is ready and completed (4)
   // and that the status is "Ok" (200)
   xhttp.onload = () => {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      let errorHandler = document.getElementById('error-handler');
-      let recipeContainer = document.getElementById('returned-recipe-container');
-      recipeContainer.innerHTML = "";
-      errorHandler.innerHTML = "";
-      if (xhttp.response.hits.length == 0) {
-        errorHandler.innerHTML = "Sorry we couldn't find what you were looking for. <br> Please enter new ingredients and try again.";
-        recipeContainer.innerHTML = "";
-        return;
-      }
-
-      let recipesWithIds = selectUniqueRecipes(xhttp.response.hits);
-      recipesWithIds = filterFaultyRecipesFromApi(recipesWithIds);
-      console.log(recipesWithIds);
-      let randomUniqueItems = getRandomUniqueItems(recipesWithIds, 8);
-      localStorage.setItem("cached-recipes", JSON.stringify(randomUniqueItems));
-
-      // loops through the 8 random recipes returned
-      for (let i = 0; i < randomUniqueItems.length; i++) {
-        renderRecipes(randomUniqueItems[i], recipeContainer);
-      }
+      handleApiResponse(xhttp.response);
     }
   };
 }
 
+function handleApiResponse(apiResponse) {
+  let errorHandler = document.getElementById('error-handler');
+  let recipeContainer = document.getElementById('returned-recipe-container');
+  recipeContainer.innerHTML = "";
+  errorHandler.innerHTML = "";
+  if (apiResponse.hits.length == 0) {
+    errorHandler.innerHTML = "Sorry we couldn't find what you were looking for. <br> Please enter new ingredients and try again.";
+    recipeContainer.innerHTML = "";
+    return;
+  }
 
-function filterFaultyRecipesFromApi(recipesWithIds) {
-  console.log(recipesWithIds);
-  return recipesWithIds.filter(item =>
-    item.Id != "b2cb2273a19b40ad4b2ee01181de2f67"
-    && item.Id != "b6059ba07ff9441a76572ba622c6ed83"
-    && item.Id != "feae839eb15c7959e1fbafa8ffb2c7df"
-    && item.Id != "24ef4d12d940a7afe7cc491561354132"
-    && item.Id != "3c9621517f60ff37eaec13587b7730ef");
+  let eightUniqueRecipes = cleanupRecipeArrayFromApi(apiResponse.hits);
+  renderRecipes(eightUniqueRecipes, recipeContainer);
+}
+
+function cleanupRecipeArrayFromApi(apiRecipes) {
+  let recipesWithIds = selectUniqueRecipes(apiRecipes);
+  recipesWithIds = filterFaultyRecipesFromApi(recipesWithIds);
+  let randomUniqueItems = getRandomUniqueItems(recipesWithIds, 8);
+  localStorage.setItem("cached-recipes", JSON.stringify(randomUniqueItems));
+  return randomUniqueItems;
 }
 
 function selectUniqueRecipes(recipeArray) {
@@ -74,12 +86,46 @@ function selectUniqueRecipes(recipeArray) {
   return randomUniqueRecipes;
 }
 
+function filterFaultyRecipesFromApi(recipesWithIds) {
+  return recipesWithIds.filter(item =>
+    item.Id != "b2cb2273a19b40ad4b2ee01181de2f67" &&
+    item.Id != "b6059ba07ff9441a76572ba622c6ed83" &&
+    item.Id != "feae839eb15c7959e1fbafa8ffb2c7df" &&
+    item.Id != "24ef4d12d940a7afe7cc491561354132" &&
+    item.Id != "3c9621517f60ff37eaec13587b7730ef");
+}
+
+function getRandomUniqueItems(array, count) {
+  const shuffledArray = array.slice(); // Create a copy to avoid modifying the original array
+  shuffleArray(shuffledArray);
+  return shuffledArray.slice(0, count);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 /**
  * renders recipes into html and appends them to the "returned-recipe-container" section.
  * @param recipe - a single recipe item
  * @param recipeContainer - the parent element which the recipe item is injected into. 
  */
-function renderRecipes(recipe, recipeContainer) {
+function renderRecipes(recipeArray, recipeContainer) {
+  // loops through the 8 random recipes returned
+  for (let i = 0; i < recipeArray.length; i++) {
+    let recipeCardDocument = buildHtmlForRecipeCard(recipeArray[i]);
+    // takes recipe card div from the created document
+    let recipeCardHtml = recipeCardDocument.body.getElementsByTagName('div')[0];
+    // takes returned recipe container and appends the recipe card to html
+    recipeContainer.appendChild(recipeCardHtml);
+    addFunctionalityToCardButton(recipeArray[i].Id);
+  }
+}
+
+function buildHtmlForRecipeCard(recipe) {
   let recipeTime = recipe.totalTime == 0 ? 30 : recipe.totalTime;
   let recipeCard = `
   <div class="recipe-card" id="${recipe.Id}">
@@ -102,31 +148,8 @@ function renderRecipes(recipe, recipeContainer) {
     </article>
   </div>
   `;
-
   // takes the text above and transforms into html
-  let recipeCardDocument = htmlParser.parseFromString(recipeCard, 'text/html');
-  // takes recipe card div from the created document
-  let recipeCardHtml = recipeCardDocument.body.getElementsByTagName('div')[0];
-  // takes returned recipe container and appends the recipe card to html
-  recipeContainer.appendChild(recipeCardHtml);
-
-  document.getElementById("btn-" + recipe.Id).addEventListener("click", function (event) {
-    event.preventDefault();
-    expandIngredients(recipe.Id);
-  });
-}
-
-function expandIngredients(recipeId) {
-  let expandContent = document.getElementById("content-" + recipeId);
-  let expandBtn = document.getElementById("btn-" + recipeId);
-  expandContent.classList.toggle("active");
-  if (expandContent.style.display === "block") {
-    expandBtn.innerHTML = "Show";
-    expandContent.style.display = "none";
-  } else {
-    expandBtn.innerHTML = "Hide";
-    expandContent.style.display = "block";
-  }
+  return htmlParser.parseFromString(recipeCard, 'text/html');
 }
 
 /**
@@ -143,38 +166,22 @@ function renderIngredients(ingredientLines) {
   return renderedIngredients;
 }
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+function addFunctionalityToCardButton(recipeId) {
+  document.getElementById("btn-" + recipeId).addEventListener("click", function (event) {
+    event.preventDefault();
+    expandIngredients(recipeId);
+  });
 }
 
-function getRandomUniqueItems(array, count) {
-  const shuffledArray = array.slice(); // Create a copy to avoid modifying the original array
-  shuffleArray(shuffledArray);
-  return shuffledArray.slice(0, count);
-}
-
-function randomButton() {
-  document.getElementById("search-term").value = "";
-  let randomItemsArray = new Array("chicken", "beef", "breadcrumbs", "tomato paste", "eggs", "sushi", "prawns", "wraps", "pasta", "rice", "fish", "peppers", "cheese", "curry");
-  let getRandomItem = randomItemsArray[Math.floor(Math.random() * randomItemsArray.length)];
-
-  callApi(getRandomItem);
-}
-
-function loadCache() {
-  let getRecipesFromCache = localStorage.getItem("cached-recipes");
-  console.log(getRecipesFromCache);
-  if (getRecipesFromCache == null) {
-    return;
-  }
-  getRecipesFromCache = JSON.parse(getRecipesFromCache);
-  let recipeContainer = document.getElementById('returned-recipe-container');
-  recipeContainer.innerHTML = "";
-  // loops through the 8 random recipes returned
-  for (let i = 0; i < getRecipesFromCache.length; i++) {
-    renderRecipes(getRecipesFromCache[i], recipeContainer);
+function expandIngredients(recipeId) {
+  let expandContent = document.getElementById("content-" + recipeId);
+  let expandBtn = document.getElementById("btn-" + recipeId);
+  expandContent.classList.toggle("active");
+  if (expandContent.style.display === "block") {
+    expandBtn.innerHTML = "Show";
+    expandContent.style.display = "none";
+  } else {
+    expandBtn.innerHTML = "Hide";
+    expandContent.style.display = "block";
   }
 }
